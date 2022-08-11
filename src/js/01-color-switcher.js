a02e41c801fb420c3109a8e83d11b1a22266cc13
Denis Orlyansky <000slim001@gmail.com>
2022-08-11 14:34:55 +0300

    fist task completed!

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+let intervalId = null;
+
+refs.startBtn.addEventListener('click', () => {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  refs.startBtn.setAttribute('disabled', '');
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  clearInterval(intervalId);
+  refs.startBtn.removeAttribute('disabled', '');
+  refs.startBtn.setAttribute('undisabled', '');
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
