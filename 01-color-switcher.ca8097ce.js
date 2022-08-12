6679a047e1d3c8fc051389ecb85d925044e9e8ed
OrlyanSKY <100310036+OrlyanSKY@users.noreply.github.com>
2022-08-12 14:43:09 +0000

    Deploying to gh-pages from @ OrlyanSKY/goit-js-hw-09@2edfb9e76182022b16d0b27ebdddad4cd0f8a78d 🚀

--- 01-color-switcher.ca8097ce.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")},e=null;t.startBtn.addEventListener("click",(function(){e=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.startBtn.setAttribute("disabled","")})),t.stopBtn.addEventListener("click",(function(){clearInterval(e),t.startBtn.removeAttribute("disabled",""),t.startBtn.setAttribute("undisabled","")}))}();
-//# sourceMappingURL=01-color-switcher.ca8097ce.js.map
