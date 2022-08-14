4d56fa3da7421d5f80330189b3a8feae65d4e041
Denis Orlyansky <000slim001@gmail.com>
2022-08-14 14:57:17 +0300

    add basic functionality to Timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,16 +1,83 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const btnStart = document.querySelector('[data-start]');
 const timer = document.querySelector('.timer');
- const timerInput = document.querySelector('#datetime-picker');
-const fp = flatpickr(timerInput, {
+const timerInput = document.querySelector('#datetime-picker');
+
+const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    const selectedData = selectedDates;
-    console.log(selectedData);
+    if (selectedDates[0] < Date.now()) {
+      Report.failure(
+        'IT was in the PAST!',
+        'Please choose a date in the future',
+        'OK'
+      );
+    }
+    timerStartTime = selectedDates[0];
   },
+  onChange(selectedDates) {
+    if (selectedDates[0] > Date.now()) {
+      btnStart.removeAttribute('disabled', '');
+    } else {
+      btnStart.setAttribute('disabled', '');
+    }
+  },
+};
+
+let timerStartTime = null;
+
+btnStart.setAttribute('disabled', '');
+const fp = flatpickr(timerInput, options);
+
+btnStart.addEventListener('click', () => {
+  timerStart(timerStartTime);
 });
+
+function timerStart(selectedDates) {
+  const timerId = setInterval(() => {
+    let differenceInTime = selectedDates - Date.now();
+
+    if (differenceInTime < 0) {
+      stopTimer(timerId);
+      return;
+    }
+    timerRender(convertMs(differenceInTime));
+  }, 1000);
+}
+function timerRender(obj) {
+  for (const key in obj) {
+    document.querySelector(`[data-${key}]`).innerHTML = String(
+      obj[key]
+    ).padStart(2, '0');
+  }
+}
+function stopTimer(timerID) {
+  clearInterval(timerID);
+  Notify.success('Timer off');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
